38ce802f89d7e6bc4dffafa1205b70babeb5448e
cekim <111842836+changeuikim@users.noreply.github.com>
2024-12-02 19:38:37 +0900

    [Lv.2] [Java] 2개 이하로 다른 비트 (4.32 ms, 108 MB)

--- 프로그래머스/레벨2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/레벨2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,12 +1,3 @@
-# [Lv.2] [2개 이하로 다른 비트](https://school.programmers.co.kr/learn/courses/30/lessons/77885?language=java) - 77885 
-
-### 구분
-
-코딩테스트 연습 > 월간 코드 챌린지 시즌2
-
-## Java 문제풀이
-
-```java
 public class Solution {
     public static long[] solution(long[] numbers) {
         long[] result = new long[numbers.length];
@@ -29,19 +20,3 @@ public class Solution {
         return result;
     }
 }
-```
-
-### 성능 요약
-
-1. 시간: 4.32 ms, 메모리: 108 MB
-
-2. 시간: 2.92 ms, 메모리: 124 MB
-3. 시간: 2.86 ms, 메모리: 119 MB
-4. 시간: 2.83 ms, 메모리: 128 MB
-5. 시간: 2.64 ms, 메모리: 111 MB
-
-### 제출 일자
-
-2024년 12월 02일 (월) 19:38
-
-> 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
