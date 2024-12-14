841d16d2f017868c325422654fccf99ab2e21c95
cekim <111842836+changeuikim@users.noreply.github.com>
2024-12-15 00:25:56 +0900

    [Gold I] [TypeScript] 구슬 탈출 4  (112 ms, 10112 KB)

---
+++ 백준/레벨Gold I/15653. 구슬 탈출 4/구슬 탈출 4.ts
@@ -0,0 +1,125 @@
+// @ts-ignore
+import fs = require('fs');
+
+// 입력 처리
+const lines = fs.readFileSync(0, 'utf-8').trim().split('\n');
+let idx = 0;
+const input = (): string => lines[idx++];
+
+type Pos = [number, number];
+
+interface State {
+  red: Pos;
+  blue: Pos;
+  moves: number;
+}
+
+const initBoard = (): { red: Pos; blue: Pos; board: string[][] } => {
+  const [N, M] = input().split(' ').map(Number);
+  const board: string[][] = [];
+  let red: Pos;
+  let blue: Pos;
+
+  for (let i = 0; i < N; i++) {
+    const line = input().split('');
+
+    for (let j = 0; j < M; j++) {
+      if (line[j] === 'R') {
+        red = [i, j];
+        line[j] = '.';
+      } else if (line[j] === 'B') {
+        blue = [i, j];
+        line[j] = '.';
+      }
+    }
+
+    board.push(line);
+  }
+
+  return { red, blue, board };
+};
+
+const move = (
+  y: number,
+  x: number,
+  dy: number,
+  dx: number,
+  board: string[][]
+): [number, number, number] => {
+  let count = 0;
+  while (board[y + dy][x + dx] !== '#' && board[y][x] !== 'O') {
+    y += dy;
+    x += dx;
+    count++;
+  }
+  return [y, x, count];
+};
+
+const solution = (): number => {
+  // 미로 초기화
+  const { red, blue, board } = initBoard();
+
+  // 방향 초기화 : 좌우상하
+  const dirs = [
+    [0, 1],
+    [0, -1],
+    [-1, 0],
+    [1, 0],
+  ];
+
+  // 큐 초기화
+  let f_stk: State[] = [];
+  let r_stk: State[] = [];
+  const visited: Set<string> = new Set();
+
+  f_stk.push({ red, blue, moves: 1 });
+  visited.add(JSON.stringify({ red, blue }));
+
+  // BFS
+  while (f_stk.length > 0 || r_stk.length > 0) {
+    const {
+      red: [ry, rx],
+      blue: [by, bx],
+      moves,
+    } = f_stk.pop();
+
+    for (const [dy, dx] of dirs) {
+      let [nBy, nBx, bCnt] = move(by, bx, dy, dx, board);
+      let [nRy, nRx, rCnt] = move(ry, rx, dy, dx, board);
+
+      // 파란구슬 실패
+      if (board[nBy][nBx] === 'O') continue;
+
+      // 빨간구슬 성공
+      if (board[nRy][nRx] === 'O') return moves;
+
+      // 위치가 겹치면 더 많이 움직인걸 뒤로
+      if (nRy === nBy && nRx === nBx) {
+        if (rCnt > bCnt) {
+          nRy -= dy;
+          nRx -= dx;
+        } else {
+          nBy -= dy;
+          nBx -= dx;
+        }
+      }
+
+      // 큐에 추가
+      const stateStr = JSON.stringify({ red: [nRy, nRx], blue: [nBy, nBx] });
+      if (!visited.has(stateStr)) {
+        r_stk.push({ red: [nRy, nRx], blue: [nBy, nBx], moves: moves + 1 });
+        visited.add(stateStr);
+      }
+    }
+
+    // 스택 교환
+    if (f_stk.length === 0) {
+      f_stk = r_stk;
+      r_stk = [];
+    }
+  }
+
+  return -1;
+};
+
+console.log(solution());
