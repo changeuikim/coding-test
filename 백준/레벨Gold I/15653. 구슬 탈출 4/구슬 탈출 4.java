9d0431f16346b71870042bbb42cc204ea58477e0
cekim <111842836+changeuikim@users.noreply.github.com>
2024-12-15 18:09:42 +0900

    [Gold I] [Java 11] 구슬 탈출 4  (108 ms, 14360 KB)

--- 백준/레벨Gold I/15653. 구슬 탈출 4/구슬 탈출 4.java
+++ 백준/레벨Gold I/15653. 구슬 탈출 4/구슬 탈출 4.java
@@ -2,165 +2,162 @@ import java.io.*;
 import java.util.*;
 
 class Pos {
-    int y, x, cnt;
+    int y, x, count;
+    
     Pos(int y, int x) {
         this.y = y;
         this.x = x;
     }
-    Pos(int y, int x, int cnt) {
+
+    Pos(int y, int x, int count) {
         this.y = y;
         this.x = x;
-        this.cnt = cnt;
+        this.count = count;
     }
 }
 
 class State {
     Pos red, blue;
-    int moves;
+    int move;
 
-    State(Pos red, Pos blue, int moves) {
+    State(Pos red, Pos blue, int move) {
         this.red = red;
         this.blue = blue;
-        this.moves = moves;
+        this.move = move;
     }
 
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
+        if (obj == null || this.getClass() != obj.getClass()) return false;
         State state = (State) obj;
-        return red.y == state.red.y &&
-               red.x == state.red.x &&
-               blue.y == state.blue.y &&
-               blue.x == state.blue.x;
+        return this.red.y == state.red.y &&
+               this.red.x == state.red.x &&
+               this.blue.y == state.blue.y &&
+               this.blue.x == state.blue.x;
     }
-
+    
     @Override
     public int hashCode() {
         return Objects.hash(red.y, red.x, blue.y, blue.x);
     }
 }
 
-public class Main {
-    static int N, M;
-    static char[][] board;
-
-    static State initBoard() throws IOException {
-        N = nextInt();
-        M = nextInt();
-        board = new char[N][M];
-        Pos red = null, blue = null;
-
-        for (int i = 0; i < N; i++) {
-            String line = nextLine();
-
-            for (int j = 0; j < M; j++) {
-                board[i][j] = line.charAt(j);
-
-                if (board[i][j] == 'R') {
-                    red = new Pos(i, j);
-                    board[i][j] = '.';
-                }
-                else if (board[i][j] == 'B') {
-                    blue = new Pos(i, j);
-                    board[i][j] = '.';
-                }
-            }
-        }
-
-        return new State(red, blue, 1);
-    }
-
-    static Pos move(int y, int x, int dy, int dx) {
+class BFS {
+    private Pos roll(int y, int x, int dy, int dx, char[][] board) {
         int count = 0;
+        // 벽에 부딪치기 전까지, 구멍에 빠질때까지
         while (board[y + dy][x + dx] != '#' && board[y][x] != 'O') {
             y += dy;
             x += dx;
-            count++;
+            count += 1;
         }
         return new Pos(y, x, count);
     }
 
-    static int BFS(State state, int[][] dirs) {
-        // 큐 초기화
+    public int solution() throws IOException {
+        // 보드, 빨강, 파랑 초기화
+        int N = nextInt(), M = nextInt();
+        char[][] board = new char[N][M];
+        Pos red = null, blue = null;
+
+        for (int i = 0; i < N; i++) {
+            char[] row = nextLine().toCharArray();
+
+            for (int j = 0; j < M; j++) {
+                if (row[j] == 'R') {
+                    red = new Pos(i, j);
+                    row[j] = '.';
+                } else if (row[j] == 'B') {
+                    blue = new Pos(i, j);
+                    row[j] = '.';
+                }
+            }
+
+            board[i] = row;
+        }
+
+        // 방향 : 좌우상하
+        int[] dy = { 0, 0, -1, 1};
+        int[] dx = { -1, 1, 0, 0};
+
+        // 큐, 방문 초기화
         Queue<State> q = new LinkedList<>();
         Set<State> visited = new HashSet<>();
 
-        q.offer(state);
-        visited.add(state);
+        State inits = new State(red, blue, 1);
+        q.offer(inits);
+        visited.add(inits);
 
+        // BFS
         while (!q.isEmpty()) {
-            State current = q.poll();
-            Pos red = current.red;
-            Pos blue = current.blue;
-            int moves = current.moves;
+            State curState = q.poll();
 
-            for (int[] dir : dirs) {
-                Pos bluePos = move(blue.y, blue.x, dir[0], dir[1]);
-                int nBy = bluePos.y, nBx = bluePos.x, bCnt = bluePos.cnt;
-
-                Pos redPos = move(red.y, red.x, dir[0], dir[1]);
-                int nRy = redPos.y, nRx = redPos.x, rCnt = redPos.cnt;
-
-                // 파란구슬 실패
-                if (board[nBy][nBx] == 'O') continue;
-
-                // 빨간구슬 성공
-                if (board[nRy][nRx] == 'O') return moves;
-
-                // 위치가 겹치면 더 많이 움직인걸 뒤로
-                if (nRy == nBy && nRx == nBx) {
-                    if (rCnt > bCnt) {
-                        nRy -= dir[0];
-                        nRx -= dir[1];
-                    } else {
-                        nBy -= dir[0];
-                        nBx -= dir[1];
-                    }
+            Pos cr = curState.red;
+            Pos cb = curState.blue;
+            int move = curState.move;
+        
+            // 좌우상하 굴리기
+            for (int i = 0; i < 4; i++) {
+                Pos nr = roll(cr.y, cr.x, dy[i], dx[i], board);
+                Pos nb = roll(cb.y, cb.x, dy[i], dx[i], board);
+        
+                // 빨간 구슬과 파란 구슬이 동시에 구멍에 빠져도 실패
+                if (board[nb.y][nb.x] == 'O') continue;
+        
+                // 빨간 구슬이 구멍에 빠지면 성공
+                if (board[nr.y][nr.x] == 'O') return move;
+        
+                // 빨간 구슬과 파란 구슬은 동시에 같은 칸에 있을 수 없다
+                if (nr.y == nb.y && nr.x == nb.x) {
+                // 빨간 구슬을 뒤로
+                if (nr.count > nb.count) {
+                    nr.y -= dy[i];
+                    nr.x -= dx[i];
                 }
-                
+                // 파란 구슬을 뒤로
+                else {
+                    nb.y -= dy[i];
+                    nb.x -= dx[i];
+                }
+                }
+        
                 // 큐에 추가
-                State nextState = new State(new Pos(nRy, nRx), new Pos(nBy, nBx), moves + 1);
-                if (!visited.contains(nextState)) {
-                    q.offer(nextState);
-                    visited.add(nextState);
-                }                
+                State nxtState = new State(nr, nb, move + 1);
+                if (visited.contains(nxtState)) continue;
+        
+                q.offer(nxtState);
+                visited.add(nxtState);
             }
-        }
-
+          }
+        
         return -1;
     }
 
-    public static void main(String[] args) throws IOException {
-        // 미로 초기화
-        State state = initBoard();
-
-        // 방향 초기화
-        int[][] dirs = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
-
-        // BFS
-        int result = BFS(state, dirs);
-
-        System.out.println(result);
-    }
-
-    static int nextInt() throws IOException {
+    int nextInt() throws IOException {
         int n = 0;
         int c;
-        while((c = System.in.read()) <= 32); // 탭 9, 개행 10, 공백 32
+        while ((c = System.in.read()) <= 32); // 탭 9, 개행 10, 공백 32
         do {
-            n = n * 10 + (c - '0'); // 현재 자리를 추가
+            n = n * 10 + (c - '0'); // 한자리씩 추가
         } while ((c = System.in.read()) > 32); // 구분자 전까지
         return n;
     }
 
-    static String nextLine() throws IOException {
-        StringBuilder sb = new StringBuilder();
+    String nextLine() throws IOException {
+        char[] buf = new char[10];
+        int idx = 0;
         int c;
-        while((c = System.in.read()) <= 32); // 탭 9, 개행 10, 공백 32
+        while ((c = System.in.read()) <= 32); // 탭 9, 개행 10, 공백 32
         do {
-            sb.append((char)c); // 현재 자리를 추가가
-        } while ((c = System.in.read()) > 10); // 개행 전까지
-        return sb.toString();
-    }    
+            buf[idx++] = (char)c; // 한글자씩 추가
+        } while ((c = System.in.read()) > 10); // 개행 전까지지
+        return new String(buf, 0, idx);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(new BFS().solution());
+    }
 }
