f5e6d50259658a61c7b7f2d943f36777f7b6fcdc
cekim <111842836+changeuikim@users.noreply.github.com>
2024-12-25 22:01:36 +0900

    [Platinum V] [C++17 (Clang)] Strongly Connected Component  (20 ms, 3708 KB)

--- 백준/레벨Platinum V/2150. Strongly Connected Component/Strongly Connected Component.cc
+++ 백준/레벨Platinum V/2150. Strongly Connected Component/Strongly Connected Component.cc
@@ -1,69 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<vector<int>> graph;
+// SCC 후보 스택, 정점의 스택 포함 여부
+stack<int> sccStack;
+vector<bool> inStack;
+// DFS 방문순서, 카운터
+vector<int> visited;
+int visitedCount;
+// SCC 목록
+vector<vector<int>> sccList;
+
+int dfs(int u) {
+    // low : u에서 역방향으로 거슬러 갈 수 있는 가장 작은 방문순서
+    visited[u] = visitedCount;
+    int low = visitedCount++;
+
+    // 스택에 u를 추가
+    sccStack.push(u);
+    inStack[u] = true;
+
+    // 인접 정점을 탐색
+    for (int v : graph[u]) {
+        // v 방문 전이면 dfs(v)후 low 갱신
+        if (visited[v] == -1) {
+            low = min(low, dfs(v)); // 두번째 low : 자식 정점의 방문 순서
+        }
+        // 방문했지만 SCC 후보라면 low 갱신
+        else if (inStack[v]) {
+            low = min(low, visited[v]); // 세번째 low : 스택에 있는 방문 순서
+        }
+    }
+
+    // low가 방문순서와 동일 : 먼저 방문된 노드로 돌아갈 수 없어서 여기가 SCC 루트
+    if (low == visited[u]) {
+        vector<int> curSCC;
+
+        // SCC 구성하기
+        while (true) {
+            int top = sccStack.top();
+            sccStack.pop();
+            inStack[top] = false;
+            curSCC.push_back(top);
+            if (top == u) break;
+        }
+        sort(curSCC.begin(), curSCC.end());
+        sccList.push_back(curSCC);
+    }
+
+    return low;
+}
+
 void solution() {
     // 정방향, 역방향 인접리스트 초기화
     int V, E;
     cin >> V >> E;
-
-    vector<vector<int>> fGraph(V + 1), rGraph(V + 1);
-
+    graph.assign(V + 1, vector<int>());
+    
     for (int i = 0; i < E; i++) {
         int u, v;
         cin >> u >> v;
-        fGraph[u].push_back(v);
-        rGraph[v].push_back(u);
+        graph[u].push_back(v);
     }
 
-    // 모든 정점에 대해 정방향 DFS
-    vector<bool> visited(V + 1, false);
-    vector<int> orderStack;
-    stack<pair<int, int>> fDFS;
+    sccStack = stack<int>();
+    inStack.assign(V + 1, false);
+    visited.assign(V + 1, -1);
+    visitedCount = 0;
+    sccList.clear();
 
-    for (int i = 1; i <= V; i++) {
-        fDFS.push({i, -1});
-    }
-
-    while (!fDFS.empty()) {
-        auto [u, state] = fDFS.top();
-        fDFS.pop();
-        if (state > -1) {
-            orderStack.push_back(u);
-            continue;
-        }
-        if (visited[u]) continue;
-
-        visited[u] = true;
-        fDFS.push({u, 0});
-        for (int v : fGraph[u]) {
-            if (!visited[v]) fDFS.push({v, -1});
-        }
-    }
-
-    // 방문 정점에 대해 역방향 DFS
-    visited.assign(V + 1, false);
-    vector<vector<int>> sccList;
-    stack<int> rDFS;
-
-    for (int i = orderStack.size() - 1; i >= 0; i--) {
-        int start = orderStack[i];
-        if (visited[start]) continue;
-
-        rDFS.push(start);
-        vector<int> curSCC;
-        while (!rDFS.empty()) {
-            int v = rDFS.top();
-            rDFS.pop();
-            if (visited[v]) continue;
-
-            visited[v] = true;
-            curSCC.push_back(v);
-            for (int u : rGraph[v]) {
-                if (!visited[u]) rDFS.push(u);
-            }
-        }
-        sort(curSCC.begin(), curSCC.end());
-        sccList.push_back(curSCC);
+    // 모든 정점에 대해 DFS
+    for (int u = 1; u <= V; u++) {
+        if (visited[u] == -1) dfs(u);
     }
 
     // 첫줄에 SCC의 개수 K를 출력
@@ -81,7 +89,7 @@ void solution() {
     }
 }
 
-int main(void) {
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     solution();
