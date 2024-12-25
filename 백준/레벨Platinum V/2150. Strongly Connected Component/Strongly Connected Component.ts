cb0e699f981f7c1bb674078de9f22417496a8bef
cekim <111842836+changeuikim@users.noreply.github.com>
2024-12-25 21:55:58 +0900

    [Platinum V] [TypeScript] Strongly Connected Component  (348 ms, 45780 KB)

---
+++ 백준/레벨Platinum V/2150. Strongly Connected Component/Strongly Connected Component.ts
@@ -0,0 +1,72 @@
+// @ts-ignore
+import fs = require('fs');
+
+const read = fs.readFileSync(0, 'utf-8');
+const lines = read.split('\n').map((line: string) => line.split(' ').map(Number));
+let lineIndex = 0;
+
+const kosaraju = () => {
+  // 정방향, 역방향 인접리스트 초기화
+  const [V, E] = lines[lineIndex++];
+  const fGraph = Array.from({ length: V + 1 }, () => [] as number[]);
+  const rGraph = Array.from({ length: V + 1 }, () => [] as number[]);
+
+  for (let i = 0; i < E; i++) {
+    const [u, v] = lines[lineIndex++];
+    fGraph[u].push(v);
+    rGraph[v].push(u);
+  }
+
+  // 모든 정점에 대해 정방향 DFS
+  let visited = Array(V + 1).fill(false);
+  const orderStack: number[] = [];
+
+  const fDFS = (u: number) => {
+    visited[u] = true;
+    for (const v of fGraph[u]) {
+      if (!visited[v]) fDFS(v);
+    }
+    // 정점방문 순서를 추가
+    orderStack.push(u);
+  };
+
+  for (let u = 1; u <= V; u++) {
+    if (!visited[u]) fDFS(u);
+  }
+
+  // 방문 정점에 대해 역방향 DFS
+  visited = Array(V + 1).fill(false);
+  const sccList: number[][] = [];
+
+  const rDFS = (v: number, curSCC: number[]) => {
+    visited[v] = true;
+    // 현재 SCC를 구성
+    curSCC.push(v);
+    for (const u of rGraph[v]) {
+      if (!visited[u]) rDFS(u, curSCC);
+    }
+  };
+
+  while (orderStack.length > 0) {
+    const v = orderStack.pop();
+    if (!visited[v]) {
+      let curSCC: number[] = [];
+      rDFS(v, curSCC);
+      sccList.push(curSCC);
+    }
+  }
+
+  // 첫줄에 SCC의 개수 K를 출력
+  // 다음 K개의 줄에는 각 줄에 하나의 SCC에 속한 정점의 번호를 출력
+  const result = [
+    sccList.length.toString(),
+    ...sccList
+      .map((scc) => scc.sort((x, y) => x - y))
+      .sort((a, b) => a[0] - b[0])
+      .map((scc) => [...scc, -1].join(' ')),
+  ].join('\n');
+
+  console.log(result);
+};
+
+kosaraju();
